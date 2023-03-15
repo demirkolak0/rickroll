a67dc3e89b03216e827689691ce093c8a5f373f8
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2023-03-16 00:30:52 +0300

    Update office.cs

--- office.cs
+++ office.cs
@@ -1,12 +1,26 @@
 using System;
-using System.Diagnostics;
+using  System.Media;
+using System.Threading;
+using System.Runtime.InteropServices;
 namespace Rickroll
 {
 	class Program
 	{
+		[DllImport("Kernel32.dll")]
+		private static extern IntPtr GetConsoleWindow();
+		[DllImport("User32.dll")]
+		private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
 		static void Main(string[] args)
-		{
-			System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+		{ 	
+			IntPtr hWnd = GetConsoleWindow();
+			if(hWnd != IntPtr.Zero)
+			{
+				ShowWindow(hWnd, 0);
+			}
+			SoundPlayer rickroll = new SoundPlayer(@"rickroll.wav");
+			rickroll.Load();
+			rickroll.PlayLooping();
+			Console.ReadKey();
 		}
 	}
 }
